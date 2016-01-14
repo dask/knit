bfd89cc85396b1475d86eca55dc79969a2d68c55
Benjamin Zaitlen <quasiben@gmail.com>
2016-01-14 11:17:49 -0500

    adding kill application and pep8 cleanup

--- rambling/core.py
+++ rambling/core.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
 import os
-import subprocess
+import re
 import requests
 import logging
 from subprocess import Popen, PIPE
@@ -42,8 +42,6 @@ class Rambling(object):
         host_port = "{}:{}".format(self.namenode, self.nm_port)
         return os.path.join("hdfs://", host_port, "jars", JAR_FILE)
 
-
-
     def start_application(self, cmd, num_containers=1, virtual_cores=1, memory=128):
         """
         Method to start a yarn app with a distributed shell
@@ -77,21 +75,21 @@ class Rambling(object):
                 str(num_containers), cmd, str(virtual_cores), str(memory)]
 
         logger.debug("Running Command: {}".format(' '.join(args)))
-        proc = subprocess.Popen(args, stdout=PIPE, stderr=PIPE)
+        proc = Popen(args, stdout=PIPE, stderr=PIPE)
         out, err = proc.communicate()
 
         logger.debug(out)
         logger.debug(err)
+
         # last string in out is applicationId
         # TODO Better JAVA Python communcation: appId, Resources, Yarn, etc.
-        appId = out.split()[-1]
+
+        appId = out.split()[-1].decode("utf-8")
+        appId = re.sub('id', '', appId)
         return appId
-
-
 
     def get_application_logs(self, app_id):
         """
-
         Parameters
         ----------
         app_id: str
@@ -113,7 +111,8 @@ class Rambling(object):
             amHostHttpAddress = data['app']['amHostHttpAddress']
         except KeyError:
             msg = "Local logs unavailable. State: {} finalStatus: {} Possibly check logs " \
-                  "with `yarn logs -applicationId`".format(data['app']['state'], data['app']['finalStatus'])
+                  "with `yarn logs -applicationId`".format(data['app']['state'],
+                                                           data['app']['finalStatus'])
             raise Exception(msg)
 
         url = "http://{}/ws/v1/node/containers".format(amHostHttpAddress)
@@ -121,8 +120,9 @@ class Rambling(object):
         data = r.json()['containers']['container']
         logger.debug(data)
 
-        #container_1452274436693_0001_01_000001
-        get_app_id_num = lambda x: "_".join(x.split("_")[1:3])
+        # container_1452274436693_0001_01_000001
+        def get_app_id_num(x):
+            return "_".join(x.split("_")[1:3])
 
         app_id_num = get_app_id_num(app_id)
         containers = [d for d in data if get_app_id_num(d['id']) == app_id_num]
@@ -146,3 +146,28 @@ class Rambling(object):
             logs[c['id']] = log
 
         return logs
+
+    def kill_application(self, application_id):
+        """
+        Method to kill a yarn application
+
+        Parameters
+        ----------
+        application_id: str
+            YARN applicaiton id
+
+        Returns
+        -------
+        bool:
+            True if successful, False otherwise.
+        """
+
+        args = ["yarn", "application", "-kill", application_id]
+
+        proc = Popen(args, stdout=PIPE, stderr=PIPE)
+        out, err = proc.communicate()
+
+        logger.debug(out)
+        logger.debug(err)
+
+        return any("Killed application" in s for s in [str(out), str(err)])
