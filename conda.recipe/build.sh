7950834a0179871614193b8b03d45d8d4c05b04e
Benjamin Zaitlen <quasiben@gmail.com>
2016-02-07 15:55:11 -0500

    removing spurious backtick

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -1,7 +1,9 @@
 unamestr=`uname`
 if [[ $unamestr == 'Linux' ]]; then
-    if [[ -z $JAVA_HOME]]; then
-        export JAVA_HOME=/opt/anaconda/bin/java
+    if [ -z $JAVA_HOME]; then
+        # Building with java-jdk on anaconda.org
+        export JAVA_HOME=/opt/anaconda/envs/_build/jre
+        export LD_LIBRARY_PATH=/opt/anaconda/envs/_build/jre/lib/amd64/:$LD_LIBRARY_PATH
     fi
 elif [[ $unamestr == 'Darwin' ]]; then
    export JAVA_HOME=$(/usr/libexec/java_home)
