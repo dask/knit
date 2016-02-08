779a4c3ccced74d163a770397932ebaca7a8a954
Benjamin Zaitlen <quasiben@gmail.com>
2016-02-07 22:38:24 -0500

    refactor xml parsing

--- knit/tests/test_utils.py
+++ knit/tests/test_utils.py
@@ -1,12 +1,11 @@
 import os
 import logging
-from lxml import etree
 
-from knit.utils import conf_find, set_logging
-from knit.exceptions import HDFSConfigException
+from knit.utils import conf_find, set_logging, parse_xml
 
 cur_dir = os.path.dirname(__file__)
 core_site = os.path.join(cur_dir, 'files', 'core-site.xml')
+yarn_site = os.path.join(cur_dir, 'files', 'yarn-site.xml')
 
 def check_docker():
     """check if inside docker container"""
@@ -18,6 +17,8 @@ inside_docker = check_docker
 def test_conf_parse():
     assert 'hdfs://knit-host:9000' == conf_find(core_site, 'fs.defaultFS')
     assert '' == conf_find(core_site, 'FOO/BAR')
+    conf = parse_xml(yarn_site, 'yarn.resourcemanager.webapp.address')
+    assert conf == {'port': '8088', 'host': 'knit-host'}
 
 
 def test_set_logging():
