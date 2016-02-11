d65c4bfd5278865fc6faa1b15c4fc69781bda708
Benjamin Zaitlen <quasiben@gmail.com>
2016-02-11 14:10:26 -0500

    change file path for anaconda.org builders

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -2,11 +2,12 @@ unamestr=`uname`
 if [[ $unamestr == 'Linux' ]]; then
     if [ -z $JAVA_HOME]; then
         # Building with java-jdk from anaconda.org
-        export JAVA_HOME=/opt/anaconda/envs/_build/jre
-        export JRE_HOME=/opt/anaconda/envs/_build/jre
-        export LD_LIBRARY_PATH=/opt/anaconda/envs/_build/jre/lib/amd64/
+        export JAVA_HOME=/opt/miniconda/envs/_build/jre
+        export JRE_HOME=/opt/miniconda/envs/_build/jre
+        export LD_LIBRARY_PATH=/opt/miniconda/envs/_build/jre/lib/amd64/:$LD_LIBRARY_PATH
     fi
 elif [[ $unamestr == 'Darwin' ]]; then
    export JAVA_HOME=$(/usr/libexec/java_home)
 fi
+java -version
 $PYTHON setup.py install mvn
